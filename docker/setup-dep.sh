9430347965344f220bdb7d5130bff209fdd01a5c
Kai Mast <kaimast@cs.cornell.edu>
2018-12-02 16:35:55 -0500

    Install cmake

--- docker/setup-dep.sh
+++ docker/setup-dep.sh
@@ -11,16 +11,16 @@ mkdir -p $HOME/local/{bin,include,lib}
 # packages
 apt-get update > /dev/null 2>&1
 apt-get install -y --no-install-recommends \
-    g++-7 sudo ca-certificates git pkg-config \
+    g++-8 sudo ca-certificates git pkg-config \
     build-essential ocaml automake autoconf libtool wget python ocamlbuild libssl-dev \
     libgmp3-dev libgflags-dev libgoogle-glog-dev google-mock googletest libgtest-dev \
     pkg-config libboost-program-options-dev libboost-python-dev unzip ninja-build \
     clang-6.0 clang-tidy-6.0 clang-format \
-    python3-pip python3-dev python3 meson python3-setuptools python3-wheel psmisc doxygen graphviz libbotan-2-dev > /dev/null 2>&1
+    python3-pip python3-dev python3 meson python3-setuptools python3-wheel psmisc doxygen graphviz libbotan-2-dev cmake > /dev/null 2>&1
 
 #SGX SDK doesn't support clang yet
-export CC=gcc-7
-export CXX=g++-7
+export CC=gcc-8
+export CXX=g++-8
 
 # sgxsdk
 git clone https://github.com/01org/linux-sgx.git > /dev/null 2>&1
